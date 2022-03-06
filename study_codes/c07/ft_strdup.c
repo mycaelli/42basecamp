241faaf146ad6d1b98daf105d48e2710dbb719fa
mycaelli <mycaelli@usp.br>
2022-03-05 23:17:40 -0300

    strdup working -> first version

--- study_codes/c07/ft_strdup.c
+++ study_codes/c07/ft_strdup.c
@@ -1,6 +1,29 @@
-char *ft_strdup(char *src);
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 char *ft_strdup(char *src)
 {
+	char *dest;
+	int i;
+	int j;
+	
+	i = 0;
+	j = -1;
+	while (src[i++] != '\0');
+	dest = malloc(i*sizeof(char));
+	dest[i] = '\0';
+	while (++j < i)
+		dest[j] = src[j];
+	return dest;
+}	
 
+int main()
+{
+	char *str = "myca";
+	printf("src: %s\n", str);
+	printf("dest: %s\n", ft_strdup(str));
+	return (0);
 }
