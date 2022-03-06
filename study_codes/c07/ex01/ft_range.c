28659727f63ba4ecb35b8e5a1e8f6a6e9de86147
mycaelli <mycaelli@usp.br>
2022-03-06 18:45:27 -0300

    working, not completely tested

--- study_codes/c07/ex01/ft_range.c
+++ study_codes/c07/ex01/ft_range.c
@@ -1,9 +1,3 @@
-// returns a int array
-// array contains the values from min to max
-// include min, exclude max
-// if min >= max -> returns NULL
-
-#include <stdio.h>
 #include <stdlib.h>
 
 int	*ft_range(int min, int max)
@@ -15,25 +9,8 @@ int	*ft_range(int min, int max)
 	if (min >= max)
 		return (NULL);	
 	range = (int *) malloc(max-min);
-//	range[0] = 30;
-//	int size = sizeof (range) / sizeof *range;
-//	printf("%d\n", max-min);
-//	printf("%d\n", size);
-	while (min < max)
-	{
-		printf("i: %d\n", i);
-		range[i++] = min++;
-		printf("i++: %d \n", i);
-	}
+       	while (min < max)
+		range[i++] = min++; 
 	return (range);
 }
 
-int main()
-{
-	int *test = ft_range(0, 5);
-//	int *test = ft_range(12, 20);
-	for(int i = 0; i < 5; i++){
-		printf("%d ", test[i]);
-	} 
-	printf("\n");
-}
