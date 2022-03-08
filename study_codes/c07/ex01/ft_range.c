f24494c88fa4f53e6ced54a197c02a0ddf5292e4
mycaelli <mycaelli@usp.br>
2022-03-08 15:51:27 -0300

    working

--- study_codes/c07/ex01/ft_range.c
+++ study_codes/c07/ex01/ft_range.c
@@ -8,29 +8,22 @@ int	*ft_range(int min, int max)
 	int range;
 
 	i = 0;
-	range = max - min-1;
+	range = max - min - 1;
 	if (min >= max)
 		return (NULL);
-	printf("aqui");	
 	if ((buffer = malloc(range * sizeof(int))) == NULL) //if NULL -> not enough memory space
 		return (NULL);	
-	printf("heree");
-//	int aux = max - min;
-//	printf("%d\n", aux);
 	while (min <= range)
 		buffer[i++] = min++;
-//	for (int j = 0; j < aux; j++)
-//	{
-//		printf("%d ", buffer[j]);
-//	}
-
 	return (buffer);
 }
 
 int main()
 {
 	int min = 0;
-	int max = 100000;
-	ft_range(min, max);
+	int max = 1000000;
+	int *test = ft_range(min, max);
+	for (int j = 0; j <= max-min-1; j++)
+		printf("%d ", test[j]);
 	printf("\n");
 }
